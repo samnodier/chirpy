5e89287a62b5a9cd8072305db84d13ff30bac0d3
Sam Nodier <sam@samnodier.com>
2026-03-12 16:56:13 -0400

    c1l5 done

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fmt.Println("Server starting on port 8080...")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("err")
+	}
 }
