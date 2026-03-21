e47e8ef3d658720208d612252fb9dd0101b743ac
Sam Nodier <sam@samnodier.com>
2026-03-21 19:39:06 -0400

    c9l4 done

--- handle_chirps_get.go
+++ handle_chirps_get.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
 	"github.com/samnodier/chirpy/internal/database"
@@ -9,6 +10,7 @@ import (
 
 func (cfg *apiConfig) handleChirpsGet(w http.ResponseWriter, r *http.Request) {
 	authorIDString := r.URL.Query().Get("author_id")
+	sortString := r.URL.Query().Get("sort")
 
 	var dbChirps []database.Chirp
 	var err error
@@ -30,6 +32,11 @@ func (cfg *apiConfig) handleChirpsGet(w http.ResponseWriter, r *http.Request) {
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		chirps = append(chirps, databaseChirpToChirp(dbChirp))
+	}
+	if sortString == "desc" {
+		sort.Slice(chirps, func(i, j int) bool { return chirps[i].CreatedAt.After(chirps[j].CreatedAt) })
+	} else {
+		sort.Slice(chirps, func(i, j int) bool { return chirps[i].CreatedAt.Before(chirps[j].CreatedAt) })
 	}
 	respondWithJSON(w, 200, chirps)
 }
